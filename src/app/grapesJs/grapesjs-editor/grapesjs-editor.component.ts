14ec654581c598a7d63a2ae7c68c2c239706af8e
Mohammad Omari <m.omari@classera.com>
2024-04-24 23:27:07 +0300

    customize grapesjs and adding some cool stuff

--- src/app/grapesJs/grapesjs-editor/grapesjs-editor.component.ts
+++ src/app/grapesJs/grapesjs-editor/grapesjs-editor.component.ts
@@ -1,57 +1,75 @@
-import { AfterContentInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { grapesjs } from 'grapesjs'
-import basicBlocks  from 'grapesjs-blocks-basic'
+import { AfterContentInit, Component, ElementRef, OnInit, ViewChild, input } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
 
-import presetWebPage  from "grapesjs-preset-webpage";
+import { grapesjs } from 'grapesjs'
+
+import basicBlocks  from 'grapesjs-blocks-basic'
 import flexBox from "grapesjs-blocks-flexbox";
 import gjsForms from 'grapesjs-plugin-forms';
 import gradient from 'grapesjs-style-gradient';
+import presetWebPage  from "grapesjs-preset-webpage";
+
 // import grapesjsLorySlider from "grapesjs-lory-slider";
 
 @Component({
   selector: 'app-grapesjs-editor',
   standalone: true,
   imports: [
-    
+    CommonModule,
+    MatButtonModule,
+    MatIcon,
   ],
   templateUrl: './grapesjs-editor.component.html',
   styleUrl: './grapesjs-editor.component.sass'
 })
 export class GrapesjsEditorComponent implements OnInit, AfterContentInit {
   editor: any;
-  htmlData: any;
   cssData: any;
+  showEditor: boolean = false;
+  changeHappend: boolean = false;
+  htmlData: string | any = input('')
 
   @ViewChild('container', { static: true }) container: ElementRef<HTMLDivElement> | undefined;
 
+
+  constructor(private sanitizer: DomSanitizer) {}
   ngAfterContentInit(): void {
-      console.log('this is the thing', this.container);
 
       this.editor = grapesjs.init({
         container: '#editor',
         fromElement: true,
         plugins: [basicBlocks, presetWebPage, gjsForms, gradient, flexBox],
-        pluginsOpts: {
-          'presetWebPage': {
-            enable: true
-          }
-        }
+        pluginsOpts: {},
 
       });
 
       this.editor.setComponents(this.htmlData);
       this.editor.setStyle(this.cssData);
 
-    // this.editor.blockManager.add('h1-block', {
-    //   label: 'Heading',
-    //   content: '<h1>Put your title here</h1>',
-    //   category: 'Basic',
-    //   attributes: {
-    //     title: 'Insert h1 block'
-    //   }
-    // });
+    this.editor.on('component:update', () => {
+      this.changeHappend = true
+    });
 
   }
+
+  
+
+  openFullSecreen = () => {
+    this.editor.Commands.run('fullscreen', { open: true }); // Open fullscreen
+  }
+
+  
+  savetemplate(){
+    const newHtmlData = this.editor.getHtml();
+    const newCssData = this.editor.getCss();
+     this.template = this.sanitizer.bypassSecurityTrustHtml(`${this.editor.getHtml()} \n <style> ${this.editor.getCss()} </style>`)
+    console.log(newHtmlData, newCssData);
+    
+  }
+  template: SafeHtml = ""
 
   ngOnInit(): void {
 
--- src/app/sections/sections-editor/sections-editor.component.ts
+++ src/app/sections/sections-editor/sections-editor.component.ts
@@ -1,16 +1,28 @@
 
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatExpansionModule} from '@angular/material/expansion';
+
 import { GrapesjsEditorComponent } from '../../grapesJs/grapesjs-editor/grapesjs-editor.component';
 
 @Component({
   selector: 'app-sections-editor',
   standalone: true,
+
   imports: [
     GrapesjsEditorComponent,
+    MatExpansionModule,
+    MatButtonModule,
+    MatIconModule
+
   ],
   templateUrl: './sections-editor.component.html',
   styleUrl: './sections-editor.component.sass'
 })
-export class SectionsEditorComponent {
 
+
+export class SectionsEditorComponent {
+  panelOpenState = false;
+ 
 }
